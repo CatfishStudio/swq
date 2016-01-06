770e7a73eefad7afb31285b30a8eb13073a2260c
CatfishStudio <somov@i.ua>
2016-01-06 20:37:29 +0300

    Параллакс космоса, планеты и ландшафта. Бордер.

--- public_html/js/game/level/level.js
+++ public_html/js/game/level/level.js
@@ -5,6 +5,7 @@ var levelStage;
 var levelPlanetID;
 var levelStarsSprite;
 var levelPlanetSprite;
+var levelLandscapeSprite;
 
 function levelCreate(planetID)
 {
@@ -15,6 +16,7 @@ function levelCreate(planetID)
     
     levelBackground();
     levelBackgroundParallaxTween();
+    levelBorder();
 }
 
 function levelRemove()
@@ -28,14 +30,18 @@ function levelBackground(planetID)
     levelStarsSprite = new PIXI.Sprite(stars1Texture); 
     levelStarsSprite.position.x = 0; 
     levelStarsSprite.position.y = 0; 
-    levelStarsSprite.scale.set(1.0); 
     levelStage.addChild(levelStarsSprite);
       
     levelPlanetSprite = new PIXI.Sprite(planetTextures[levelPlanetID][1]); 
     levelPlanetSprite.position.x = 300; 
     levelPlanetSprite.position.y = 100;
-    console.log(levelPlanetSprite.scale);
     levelStage.addChild(levelPlanetSprite);
+    
+    levelLandscapeSprite = new PIXI.Sprite(planetTextures[levelPlanetID][2]);
+    levelLandscapeSprite.position.x = 0; 
+    levelLandscapeSprite.position.y = 0; 
+    levelLandscapeSprite.alpha = 0.0;
+    levelStage.addChild(levelLandscapeSprite);
 }
 
 function levelBackgroundParallaxTween()
@@ -52,6 +58,68 @@ function levelBackgroundParallaxTween()
             .to({x: 300, y: 0}, 10000, createjs.Ease.getPowInOut(3))
             .to({x: 300, y: 100}, 10000, createjs.Ease.getPowInOut(3));
             
+    createjs.Tween.get(levelLandscapeSprite, {loop: true}) 
+            .to({x: -50, y: 0}, 10000, createjs.Ease.getPowInOut(3))
+            .to({x: -50, y: -50}, 10000, createjs.Ease.getPowInOut(3))
+            .to({x: 0, y: -50}, 10000, createjs.Ease.getPowInOut(3))
+            .to({x: 0, y: 0}, 10000, createjs.Ease.getPowInOut(3))
+            .to({alpha: 1.0}, 10000, createjs.Ease.getPowInOut(3))
+            .to({x: -50, y: 0}, 10000, createjs.Ease.getPowInOut(3))
+            .to({x: -50, y: -50}, 10000, createjs.Ease.getPowInOut(3))
+            .to({x: 0, y: -50}, 10000, createjs.Ease.getPowInOut(3))
+            .to({x: 0, y: 0}, 10000, createjs.Ease.getPowInOut(3))
+            .to({alpha: 0.0}, 10000, createjs.Ease.getPowInOut(3));
+}
+
+function levelBorder()
+{
+    var graphics = new PIXI.Graphics(); 
+
+    graphics.lineStyle(2, 0x0000FF, 1);
+    graphics.beginFill(0x000000, 0);
+    graphics.drawRect(10, 10, 840, 710);
+
+    graphics.lineStyle(0);
+    graphics.beginFill(0xFFFFFF, 1);
+    graphics.drawCircle(5, 600,4);
+    graphics.endFill();
+
+    graphics.lineStyle(2, 0xFFFFFF, 1);
+    graphics.moveTo(5,600);
+    graphics.lineTo(5, 725);
+    graphics.moveTo(5,725);
+    graphics.lineTo(650, 725);
+    graphics.moveTo(650,725);
+    graphics.lineTo(670, 710);
+    graphics.moveTo(670,710);
+    graphics.lineTo(840, 710);
+
+    graphics.lineStyle(0);
+    graphics.beginFill(0xFFFFFF, 1);
+    graphics.drawCircle(840, 710,4);
+    graphics.endFill();
+
+    graphics.lineStyle(0);
+    graphics.beginFill(0xFFFFFF, 1);
+    graphics.drawCircle(20, 20,4);
+    graphics.endFill();
+
+    graphics.lineStyle(2, 0xFFFFFF, 1);
+    graphics.moveTo(20,20);
+    graphics.lineTo(370, 20);
+    graphics.moveTo(370,20);
+    graphics.lineTo(390, 5);
+    graphics.moveTo(390,5);
+    graphics.lineTo(855, 5);
+    graphics.moveTo(855,5);
+    graphics.lineTo(855, 150);
+
+    graphics.lineStyle(0);
+    graphics.beginFill(0xFFFFFF, 1);
+    graphics.drawCircle(855, 150,4);
+    graphics.endFill();
+
+    levelStage.addChild(graphics);
 }
 
 /* == КОНЕЦ ФАЙЛА ========================================================== */

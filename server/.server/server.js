a653968e7e195f0f3c7521861552a18e58714b5f
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2017-02-10 09:16:56 +0300

    add server for test

---
+++ server/.server/server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var nodeStatic = require('node-static');
+var url = require('url'); 
+var file = new nodeStatic.Server('.');
+
+function onRequest(request, response)       // Получаем запрос
+{
+    file.serve(request, response);
+}
+
+function start(){
+    http.createServer(onRequest).listen(8080);
+    console.log('[SERVER]:', 'Server running on http://127.0.0.1:8080');
+}
+
+exports.start = start;
